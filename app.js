def4e388271696f186e99e5b9593aef61e02a087
anus26 <anusraza268gmail.com>
2024-12-04 11:11:47 -0800

    modified

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 
-  const navabar =document.getElementsByClassName("navbar")
+  // const navabar =document.getElementsByClassName("navbar")
 
-  window.addEventListener("scroll ", function(){
-    if (window.scrollY> 50) {
-        navabar.classList.add("scrolled")
+  // window.addEventListener("scroll ", function(){
+  //   if (window.scrollY> 50) {
+  //       navabar.classList.add("scrolled")
         
+  //   }else{
+  //       navabar.classList.remove('scrolled')
+  //   }
+  // })
+  const  navabar=document.querySelector("nav")
+  window.addEventListener("scroll",function(){
+    if (window.screenY> 50) {
+      navabar.classList.add("scrolled")
+      
     }else{
-        navabar.classList.remove('scrolled')
+      navabar.classList.remove("remove")
     }
   })
   
